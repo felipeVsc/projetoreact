cfa715934c44330f784edadc4333b98a5d08ba07
felipevsc <felipevasconceloslpz@gmail.com>
2018-07-13 18:39:40 -0300

    v 0.1

---
+++ src/Fundo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Fundo extends Component{
+    render() {
+        return (
+            <div>
+
+                
+                <h1 className="espacamentoEsquerdo">Idealize aqui um nome bem massa</h1>
+                <div className="fundo">
+                
+                <p className="espacamentoSair">Teste</p>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Fundo;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Assuntos from './Assuntos';
+import Fundo from './Fundo';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div><App /><Assuntos/></div>, document.getElementById('root'));
+
+ReactDOM.render(<div><Fundo/><App /><Assuntos/></div>, document.getElementById('root'));
 registerServiceWorker();
